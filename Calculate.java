01b1eaa95830a7de9c9cfe771248f469013fca12
Scoupe228 <mrmusbest@mail.ru>
2017-05-26 17:24:00 +0400

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,18 @@
 public class Calculate{
          public static void main(String[] arg) {
              System.out.println("Calculate...");
+             int first = Integer.valueOf(arg[0]);
+             int second = Integer.valueOf(arg[1]);
+             int summ = first + second;
+             System.out.println("sum "+ summ);
+             int third = Integer.valueOf(arg[2]);
+             int fourth = Integer.valueOf(arg[3]);
+             int minus = third - fourth;
+             System.out.println("minus "+ minus);
+             int fifth = Integer.valueOf(arg[4]);
+             int sixth = Integer.valueOf(arg[5]);
+             int multiply = fifth * sixth;
+             System.out.println("multiply "+ multiply);
          }
 
 }
